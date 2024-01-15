1d5f7deb5140622c6bdd404949abd882d4b3be42
rankangkang <rankangkang@foxmail.com>
2024-01-16 00:01:39 +0800

    feat: 增加deepClone

---
+++ examples/deepclone/index.js
@@ -0,0 +1,21 @@
+function deepClone(obj) {
+  if (typeof obj !== 'object') {
+    return obj
+  }
+
+  const res = obj instanceof Array ? [] : {}
+  for (const key in obj) {
+    res[key] = deepClone(obj[key])
+  }
+
+  return res
+}
+
+const target = {
+  name: "kk",
+  age: 24,
+  likes: [1, 2, "3", { key: 5 }, [7, 8]],
+}
+
+const nextTarget = deepClone(target)
+console.log(JSON.stringify(nextTarget, null, "  "))
