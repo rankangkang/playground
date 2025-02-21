0c5a106e02ee71650852a0534a36dbf4ab7d3031
kk <cmkangkang@foxmail.com>
2025-02-21 12:24:49 +0800

    feat: add drafts folder

---
+++ snippets/nodejs/argv.js
@@ -0,0 +1,16 @@
+// node --harmony argv.js --version --test="a" -h -a name
+console.log(process.argv); 
+/**
+[
+  '/Users/cmkk/.nvm/versions/node/v18.20.6/bin/node',
+  '/Volumes/kksn770/dev/cmkk/playground/snippets/nodejs/argv.js',
+  '--version',
+  '--test=a',
+  '-h',
+  '-a',
+  'name'
+]
+ */
+
+console.log(process.execArgv); // [ '--harmony' ]
+console.log(process.argv0) // node
