58c04ff49face6ed8955fdea38bc3f7ba5c61ae0
kk <cmkangkang@foxmail.com>
2025-02-27 21:53:12 +0800

    feat: add leetcode exercise

---
+++ snippets/leetcode/无重复最长子串.js
@@ -0,0 +1,27 @@
+/**
+ * tag: 简单
+ * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长 子串 的长度。
+ */
+
+/**
+ * 
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+  if (s.length <= 1) {
+    return s.length;
+  }
+
+  let r = 0;
+  // 转数组，方便使用
+  const chars = s.split('');
+  for (let i = 0, j = 1; j < s.length; j++) {
+    while (chars.slice(i, j).includes(chars[j])) {
+      i++;
+    }
+    r = Math.max(r, j - i + 1)
+  }
+
+  return r;
+};
--- snippets/leetcode/最长连续序列.js
+++ snippets/leetcode/最长连续序列.js
@@ -28,3 +28,41 @@ var longestConsecutive = function(nums) {
 
   return res;
 };
+
+/**
+ * 暴力求解
+ * @param {number[]} nums
+ */
+let longestConsecutive2 = function (nums) {
+  if (!nums) {
+    return 0
+  }
+
+  if (nums.length <= 1) {
+    return nums.length
+  }
+
+  // 通过信标一个个找
+  nums = Array.from(new Set(nums)).sort((a, b) => a - b)
+  let longest = 1
+  for (let pivot = 0; pivot < nums.length; pivot++) {
+    let val = nums[pivot]
+    let idx = pivot + 1
+    while (nums[idx] === val + 1) {
+      val = nums[idx]
+      idx += 1
+    }
+    const len = idx - pivot
+    longest = Math.max(longest, len)
+    if (longest + pivot >= nums.length) {
+      break
+    }
+  }
+
+  return longest
+}
+
+console.log(longestConsecutive([100, 4, 200, 1, 3, 2]))
+console.log(
+  longestConsecutive([100, 4, 200, 1, 1, 3, 2, 5, 6, 8, 9, 9, 10, 11, 12, 13, 14, 15, 16, 17]),
+)
