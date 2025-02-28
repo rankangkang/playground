7d697db32e561ee26d081b846b8f944e3357968c
rankangkang <rankangkang@foxmail.com>
2025-02-28 21:08:00 +0800

    feat: add leetcode exercise

---
+++ snippets/leetcode/最小栈.js
@@ -0,0 +1,56 @@
+
+/**
+ * tag: 中等
+ * 设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+ * 实现 MinStack 类:
+ * MinStack() 初始化堆栈对象。
+ * void push(int val) 将元素val推入堆栈。
+ * void pop() 删除堆栈顶部的元素。
+ * int top() 获取堆栈顶部的元素。
+ * int getMin() 获取堆栈中的最小元素。
+ */
+
+/**
+ * 难度在于如何记录最小值，要求 O(1) 的复杂度，那么通过排序查找是不行的，需要引入一个辅助栈，用于记录栈每个状态的最小值。
+ */
+var MinStack = function () {
+  if (!new.target) {
+    throw new TypeError('calling MinStack constructor without new is invalid')
+  }
+  this.stack = [];
+  // 存储时间切片（下标 i）时，stack 的最小值。
+  this.minStack = [];
+};
+
+/** 
+* @param {number} val
+* @return {void}
+*/
+MinStack.prototype.push = function (val) {
+  this.stack.push(val)
+  // 比较当前值与前面的栈内元素的最小值，插入
+  const lastMinVal = this.minStack[this.minStack.length - 1] ?? Number.MAX_SAFE_INTEGER;
+  this.minStack.push(Math.min(lastMinVal, val))
+};
+
+/**
+* @return {void}
+*/
+MinStack.prototype.pop = function () {
+  this.stack.pop()
+  this.minStack.pop()
+};
+
+/**
+* @return {number}
+*/
+MinStack.prototype.top = function () {
+  return this.stack[this.stack.length - 1]
+};
+
+/**
+* @return {number}
+*/
+MinStack.prototype.getMin = function () {
+  return this.minStack[this.minStack.length - 1]
+};
---
+++ snippets/leetcode/每日温度.js
@@ -0,0 +1,26 @@
+/**
+ * tag: 中等，非暴力解法感觉偏难，需要转换下思路
+ * 给定一个整数数组 temperatures ，表示每天的温度，返回一个数组 answer ，其中 answer[i] 是指对于第 i 天，下一个更高温度出现在几天后。如果气温在这之后都不会升高，请在该位置用 0 来代替。
+ */
+
+/**
+ * 引入单调栈（单调递减），当遍历到的元素比栈顶的下标代表的值大时，说明栈顶下标的最近更高温度就是当前遍历到的元素（因为栈是先进后出）
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function (temperatures) {
+  const len = temperatures.length;
+  const res = new Array(len).fill(0);
+  // 存储下标的栈，栈顶到栈底单调递减 
+  const stack = [];
+  for (let i = 0; i < len; i++) {
+    const temperature = temperatures[i];
+    while (stack.length > 0 && temperature > temperatures[stack[stack.length - 1]]) {
+      const prevIndex = stack.pop();
+      res[prevIndex] = i - prevIndex;
+    }
+    stack.push(i)
+  }
+
+  return res;
+};
