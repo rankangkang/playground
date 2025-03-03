933ba91d26939776a81bbeb7e552c765d82b794b
rankangkang <rankangkang@foxmail.com>
2025-03-03 18:40:25 +0800

    feat: leetcode exercise

---
+++ snippets/leetcode/对称二叉树.js
@@ -0,0 +1,33 @@
+/**
+ * tag: 简单
+ * 判断二叉树是否中心轴对称
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function (root) {
+  return compare(root.left, root.right)
+};
+
+function compare(left, right) {
+  if (!left && !right) {
+      return true
+  }
+
+  if (!left || !right) {
+      return false
+  }
+
+  const isValEqual = left.val === right.val
+  return isValEqual && compare(left.left, right.right) && compare(left.right, right.left)
+}
