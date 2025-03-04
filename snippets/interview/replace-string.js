26e05bb7dba60df5a21b4142571276db7b16fd6e
kk <cmkangkang@foxmail.com>
2025-03-04 22:50:19 +0800

    feat: add leetcode exercise

---
+++ snippets/interview/replace-string.js
@@ -0,0 +1,38 @@
+function replaceString(input, target) {
+  const index = findIndex(input, target)
+  if (index < 0) {
+    return input;
+  }
+
+  let r
+  if (index === 0) {
+    r = input.slice(target.length)
+  } else {
+    r = input.slice(0, index) + input.slice(index + target.length)
+  }
+
+  return replaceString(r, target)
+}
+
+function findIndex(input, target) {
+  const targetLen = target.length;
+  const end = input.length - targetLen
+
+  for (let i = 0; i < end; i++) {
+    let match = true
+    for (j = 0; j < targetLen; j++) {
+      if (input[i + j] !== target[j]) {
+        match = false
+        break;
+      }
+    }
+    if (match) {
+      return i
+    }
+  }
+
+  return -1;
+}
+
+// 可以用 String.prototype.replace + 正则 优化
+console.log(replaceString('aaacccb', 'ac'))
---
+++ snippets/leetcode/搜索插入位置.js
@@ -0,0 +1,54 @@
+/**
+ * @param {number[]} nums 已排序的数组
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+  // 递归二分查找
+  function doSearch(start, end) {
+    if (start > end) {
+      return start;
+    }
+
+    const mid = Math.floor((start + end) / 2)
+    const midNum = nums[mid]
+    if (midNum === target) {
+      return mid
+    } else if (midNum > target) {
+      // target 在左边
+      return doSearch(start, mid - 1)
+    } else {
+      // target 在右边
+      return doSearch(mid + 1, end)
+    }
+  }
+
+  return doSearch(0, nums.length - 1)
+};
+
+console.log(searchInsert([1, 3, 5, 6], 2))
+
+
+// 迭代的方式，标准二分查找
+function searchInsertIterate(nums, target) {
+  let l = 0, r = nums.length - 1;
+  while(l <= r) {
+    const mid = Math.floor((l + r) / 2)
+    const midNum = nums[mid]
+    if (midNum === target) {
+      return mid
+    }
+
+    if (midNum > target) {
+      // 在左边
+      r = mid - 1
+    } else {
+      l = mid + 1
+    }
+  }
+
+  // 为什么是 l？针对 l === r 时进行讨论
+  return l
+}
+
+console.log(searchInsertIterate([1, 3, 5, 6], 2))
